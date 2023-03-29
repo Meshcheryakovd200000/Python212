c4fc16beb328f7ce7dda5bc9e4695afe2a9b7e3f
Meshcheryakovd200000 <test@mail.ru>
2023-03-29 18:17:23 +0300

    29homework_python

--- 29homework_python/homework29_python.py
+++ 29homework_python/homework29_python.py
@@ -1,25 +1,110 @@
-class Shape:
-    def __init__(self, color, object_area, perimeter):
-        self.color = color
-        self.object_area = object_area
-        self.perimeter = perimeter
+from abc import abstractmethod
+from math import sqrt
 
+
+class Shape:
+    def __init__(self, color):
+        self.color = color
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+    @abstractmethod
+    def object_area(self):
+        pass
+
+    @abstractmethod
+    def painting(self):
+        pass
+
+    @abstractmethod
     def info(self):
-        print(f"{self.color} {self.object_area} {self.perimeter}")
+        object_area = self.object_area()
+        perimeter = self.perimeter()
+        print(f"Площадь: {object_area}", end="")
+        print(f"\nПериметр: {perimeter}")
 
 
 class Square(Shape):
-    def __init__(self, side, color, object_area, perimeter):
-        self.side = side
-        super().__init__(color, object_area, perimeter)
+    def __init__(self, side1, color):
+        self.side1 = side1
+        super().__init__(color)
+
+    def perimeter(self):
+        return self.side1 * 4
+
+    def object_area(self):
+        return self.side1 ** 2
 
     def info(self):
-        print(f"Сторона: {self.side}\nЦвет: {self.color}\nПлощадь: {self.object_area}\nПериметр: {self.perimeter} ")
+        print("===Квадрат===")
+        object_area = self.object_area()
+        perimeter = self.perimeter()
+        print(f"Сторона: {self.side1}\nЦвет: {self.color}\nПлощадь: {object_area}\nПериметр: {perimeter} ")
+
+    def painting(self):
+        for j in range(self.side1):
+            print("*" * self.side1)
+
+
+class Rectangle(Shape):
+    def __init__(self, side1, side2, color):
+        self.side1 = side1
+        self.side2 = side2
+        super().__init__(color)
+
+    def perimeter(self):
+        return (self.side1 + self.side2) * 2
+
+    def object_area(self):
+        return self.side1 * self.side2
+
+    def info(self):
+        print("\n===Прямоугольник===")
+        object_area = self.object_area()
+        perimeter = self.perimeter()
+        print(
+            f"Длина: {self.side1}\nШирина: {self.side2}\nЦвет: {self.color}\nПлощадь: {object_area}\nПериметр: {perimeter} ")
+
+    def painting(self):
+        for j in range(self.side1):
+            print("*" * self.side2)
+
+
+class Triangle(Shape):
+    def __init__(self, side1, side2, side3, color):
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+        super().__init__(color)
+
+    def perimeter(self):
+        return self.side1 + self.side2 + self.side3
+
+    def object_area(self):
+        p = (self.side1 + self.side2 + self.side3) / 2
+        # print(p)
+        return round(sqrt(p * (p - self.side1) * (p - self.side2) * (p - self.side3)), 2)
+
+    def info(self):
+        print("\n===Треугольник===")
+        object_area = self.object_area()
+        perimeter = self.perimeter()
+        print(
+            f"Сторона1: {self.side1}\nСторона2: {self.side2}\nСторона3: {self.side3}\nЦвет: {self.color}\nПлощадь: {object_area}\nПериметр: {perimeter} ")
+
+    def painting(self):
+        for j in range(1, self.side2 * 2, 2):
+            print(('*' * j).center(self.side2 * 2))
 
 
 group = [
-    Square(3, "red", 9, 12)
+    Square(3, "red"),
+    Rectangle(3, 7, "green"),
+    Triangle(11, 6, 6, "yellow")
 ]
 
 for i in group:
     i.info()
+    i.painting()
