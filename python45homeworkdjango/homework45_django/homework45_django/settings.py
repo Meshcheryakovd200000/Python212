629f96f36b808cc03e106d783520eb54dd486302
Meshcheryakovd200000 <test@mail.ru>
2023-06-03 20:57:14 +0300

    45homework_py

--- python45homeworkdjango/homework45_django/homework45_django/settings.py
+++ python45homeworkdjango/homework45_django/homework45_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'generator45hw.apps.Generator45HwConfig',
 ]
 
 MIDDLEWARE = [
