c8ebb33681072945c78016760be9a20e6fdc4609
Meshcheryakovd200000 <test@mail.ru>
2023-08-03 21:49:28 +0300

    gitignore

--- 42homework_python/42homework_python2.py
+++
@@ -1,9 +0,0 @@
-from jinja2 import Environment, FileSystemLoader
-
-file_loader = FileSystemLoader('templates')
-env = Environment(loader=file_loader)
-
-tm = env.get_template('about2.html')
-
-msg = tm.render()
-print(msg)
